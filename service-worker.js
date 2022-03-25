cc1f8b4ec41c73069aa510ee83d39a6e2b0b6da3
Aurelia Segarra <av.segarra@gmail.com>
2022-03-25 15:17:31 +0100

    :construction: Intercept HTTP request via service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,11 @@
-console.log("Hello form service-worker");
+self.addEventListener('install', event => {
+    console.log('install event', event);
+});
+
+self.addEventListener('activate', event => {
+    console.log('activate', event);
+});
+
+self.addEventListener('fetch', event => {
+    console.log('fetch event', event.request.url);
+})
