93e0bf30f2f0d5550b2e1cfecbd5453b6daf63f7
Aurelia Segarra <av.segarra@gmail.com>
2022-03-25 16:39:35 +0100

    :construction: Custom response when offline

--- service-worker.js
+++ service-worker.js
@@ -7,5 +7,12 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+    if (!navigator.online) {
+        const headers = { headers: { 'content-Type': 'text/html;charset=utf-8'}};
+        event.respondWith(new Response('<h1>No network connection</h1><div>Application en mode dégradé. Veuillez vous connecter !</div>', headers));
+    }
     console.log('fetch event', event.request.url);
-})
+});
+
+
+
