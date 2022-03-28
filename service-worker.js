8beff2878e81acbdcbe30a48b68ae939639a4102
Aurelia Segarra <av.segarra@gmail.com>
2022-03-28 18:37:08 +0200

    :construction: Manage caching from the service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,18 @@
+const cacheName = 'veille-techno-1.0';
+
 self.addEventListener('install', event => {
     console.log('install event', event);
+    caches.open(cacheName).then(cache => {
+        cache.addAll([
+            'index.html', 
+            'main.js',
+            'vendors/bootstrap4.min.css',
+            'contact.html',
+            'contact.js',
+            'add_techno.html',
+            'add_techno.js'
+        ]);
+    })
 });
 
 self.addEventListener('activate', event => {
